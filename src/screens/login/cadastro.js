6c7a19db22e4ca1ceadbabdceca8b682548f3e14
BrNogueira <40205786+BrNogueira@users.noreply.github.com>
2018-06-25 09:03:38 -0300

    Add files via upload

--- src/screens/login/cadastro.js
+++ src/screens/login/cadastro.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { View, TextInput, Button } from 'react-native';
 
 export default props => (
-    <View style={{ flex: 1, padding: 10 }}>
-        <View style={{ flex: 4, justifyContent: 'center' }}>
-			<TextInput placeholder='nome' />
-			<TextInput placeholder='email' />
-			<TextInput placeholder='Senha' />
-			</View>
-			<View stile={{ flex: 1  }}> 
- 	        <Button title="Cadastrar" color="#115E54" onPress={() => false} />
+   <View style={{ flex: 1, padding: 10 }}>
+    <View style={{ flex: 4, justifyContent: 'center' }}>
+		<TextInput placeholder='nome' />
+		<TextInput placeholder='email' />
+		<TextInput placeholder='Senha' />
 	</View>
-	
-	);
+		<View stile={{ flex: 1  }}> 
+	    <Button title="Cadastrar" color="#115E54" onPress={() => false} />
+	</View>
+</View>
+
+);
